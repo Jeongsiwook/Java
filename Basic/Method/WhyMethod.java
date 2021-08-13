bd443e1a6377e2b2e7a1aebf0319fb585f412f6c
Jeongsiwook <jsu0638@naver.com>
2021-08-13 13:24:31 +0900

    Create WhyMethod.java

---
+++ Basic/Method/WhyMethod.java
@@ -0,0 +1,20 @@
+
+public class WhyMethod {
+	public static void printTwoTimesA() {
+		System.out.println("-");
+		System.out.println("a");
+		System.out.println("a");
+	}
+  
+	public static void main(String[] args) {
+		
+		// 100000000
+		printTwoTimesA();
+		// 100000000
+		printTwoTimesA();
+		// 100000000
+		printTwoTimesA();
+
+	}
+
+}
